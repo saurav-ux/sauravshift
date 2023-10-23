a507c16c2d94b186cc0a720bdb95f7217e18be42
Saurav Anand <saurav.anand@auctopustechnologies.com>
2023-10-23 16:48:52 +0530

    vercel

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import './Database/connection.js'
 import shiftRouter from "./Database/Model/Router/shiftRouter.js";
 const app = express()
 app.use(cors())
+app.use(cors({
+    origin:['https://deploy-mern-1whq.vercel.app'],
+    methods:['POST','GET','DELETE'],
+    credentials:true
+  }))
 const PORT= 5007;
 app.use(express.json());
 app.use('/shift',shiftRouter)
